db352398c15199517e16caa33714fd1c090629c0
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-04-28 00:04:30 +0530

    Request and Recive intial

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink-brownie-contracts/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBase {
 
     address payable[] public players; // List of participants who are participating
     uint256 public entryFeeUsd;
@@ -15,11 +16,18 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
-
-    constructor(address _priceFeedAddress) public{
+    uint public fee = 0.1 * 10 ** 18;
+    
+    // We are changing the fee because while interacting with different networks the fee might be different so we'll updated that
+    // while deploying the contract
+    constructor(address _priceFeedAddress, address _vrfCoordinator, address _link, uint256 _fee, string memory _keyhash)
+     public
+     VRFConsumerBase(_vrfCoordinator, _link)
+     {
         entryFeeUsd  = 50 * (10 ** 18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee=_fee;
     }
 
     function enter() public payable{
@@ -43,7 +51,23 @@ contract Lottery is Ownable {
         lottery_state = LOTTERY_STATE.OPEN;
     }
 
-    function endLottery() public onlyOwner{
-        
+    // Predictable way of choosing a (not so)random number and it is giving MINERS the ability to win the lottery
+    // function endLottery() public onlyOwner{
+    //     uint256 ( 
+    //        keccak256(
+    //             abi.encodePacked(
+    //                 nonce, // Is predictable(aka transaction number in this case)
+    //                 msg.sender, // msg.sender is predictable
+    //                 block.difficulty, // Can be manupulated by the miners
+    //                 block.timestamp // timestamp is predictable
+    //             )
+    //         )
+    //     ) % players.length;
+    // }
+
+    // Keyhash uniquely identifies the Chainlink node we are going to use to get our random number
+    function endLottery(string memory keyhash) public onlyOwner{
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        requestId = requestRandomness(keyhash, fee);
     }
 }
